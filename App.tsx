46cea88202abf16b6b10c959345a9a8fde286f87
lucasnetworkmkt <moveissv@gmail.com>
2026-02-16 21:23:29 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import TargetAudience from './components/TargetAudience';
+import Strategy from './components/Strategy';
 import Features from './components/Features';
+import Portfolio from './components/Portfolio';
 import Process from './components/Process';
 import WhyChooseUs from './components/WhyChooseUs';
 import Footer from './components/Footer';
@@ -14,7 +16,9 @@ const App: React.FC = () => {
       <Header />
       <Hero />
       <TargetAudience />
+      <Strategy />
       <Features />
+      <Portfolio />
       <Process />
       <WhyChooseUs />
       <Footer />
